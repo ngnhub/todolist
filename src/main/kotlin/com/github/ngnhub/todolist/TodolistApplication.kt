d0a023a63ea8666c75583b21f6d303603c9f71d0
AlexN <anagovitsyn@gmail.com>
2023-12-23 08:16:02 +0100

    add logger and init controller

--- src/main/kotlin/com/github/ngnhub/todolist/TodolistApplication.kt
+++ src/main/kotlin/com/github/ngnhub/todolist/TodolistApplication.kt
@@ -1,5 +1,7 @@
 package com.github.ngnhub.todolist
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -9,3 +11,5 @@ class TodolistApplication
 fun main(args: Array<String>) {
     runApplication<TodolistApplication>(*args)
 }
+
+fun <T : Any> T.logger() : Logger = LoggerFactory.getLogger(javaClass)
---
+++ src/main/kotlin/com/github/ngnhub/todolist/controller/TodoController.kt
@@ -0,0 +1,29 @@
+package com.github.ngnhub.todolist.controller
+
+import com.github.ngnhub.todolist.logger
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/")
+class TodoController {
+
+    @PostMapping
+    fun create() {
+        logger().info("Creates the item")
+    }
+
+    @GetMapping("/{id}")
+    fun getBy(@PathVariable id: Long): Any {
+        return "Dummy item"
+    }
+
+    @PutMapping
+    fun markAs(status: ItemStatus) {
+        logger().info("Changing status to {}", status)
+    }
+
+    enum class ItemStatus {
+        DONE, DELETED
+    }
+}
