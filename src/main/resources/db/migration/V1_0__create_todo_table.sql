9c34eafe81102a968ded51919316afa297b18557
AlexN <anagovitsyn@gmail.com>
2023-12-23 10:18:59 +0100

    init flyWay migration

---
+++ src/main/resources/db/migration/V1_0__create_todo_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS todo_item
+(
+    id             BIGINT PRIMARY KEY,
+    title          TEXT      NOT NULL,
+    description    TEXT,
+    created_at     TIMESTAMP NOT NULL,
+    complete_until TIMESTAMP
+);
